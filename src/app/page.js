a6e8c44fcd4f98dfd6b2bb660f700df8930454d0
Ahasan Ullah <ahasanullah451@gmail.com>
2025-01-24 18:36:34 +0600

    homepage modified

--- src/app/page.js
+++ src/app/page.js
@@ -9,16 +9,13 @@ const page = async () => {
       <h1 className="text-3xl font-bold text-center mb-8">Blog Posts</h1>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 p-4">
         {posts.map((post) => (
-          <div key={post.id} className="card border">
-            <div className="card-body">
+          <div key={post.id} className="card border h-full">
+            <div className="card-body flex flex-col flex-grow">
               <h2 className="card-title">{post.title}</h2>
-              <p>
-                {post.body.length > 100
-                  ? `${post.body.slice(0, 100)}...`
-                  : post.body}
-              </p>
-              <div className="card-actions justify-end">
-                <Link href={`blog/${post.id}`} className="btn btn-neutral">Read More</Link>
+              <div className="card-actions justify-end mt-auto">
+                <Link href={`blog/${post.id}`} className="btn btn-neutral">
+                  Read More
+                </Link>
               </div>
             </div>
           </div>
